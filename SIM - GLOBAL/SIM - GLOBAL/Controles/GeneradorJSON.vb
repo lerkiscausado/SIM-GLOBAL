0a67ad0b0179f537f5f1e3dfd7479d9a1633a5a4
Merinca <cristian.merinoav@hotmail.com>
2026-07-09 13:45:41 -0500

    Actualizacion del control GeneradorJSON
    Cambio de nombre del campo nomComplicacionCIE11 por nomCodComplicacionCIE11

--- SIM - GLOBAL/SIM - GLOBAL/Controles/GeneradorJSON.vb
+++ SIM - GLOBAL/SIM - GLOBAL/Controles/GeneradorJSON.vb
@@ -105,7 +105,7 @@ Public Class GeneradorJSON
                         NULL AS nomCodDiagnosticoRelacionadoCIE11,
                         NULL AS codComplicacion,
                         NULL AS codComplicacionCIE11,
-                        NULL AS nomComplicacionCIE11,
+                        NULL AS nomCodComplicacionCIE11,
                         detalle_orden.valor AS valorServicio, 
                         '05' AS conceptoRecaudo, 
                         detalle_orden.copago AS valorPagoModerador, 
@@ -145,7 +145,7 @@ Public Class GeneradorJSON
                     proc("nomCodDiagnosticoRelacionadoCIE11") = Nothing
                     proc("codComplicacion") = Nothing
                     proc("codComplicacionCIE11") = Nothing
-                    proc("nomComplicacionCIE11") = Nothing
+                    proc("nomCodComplicacionCIE11") = Nothing
                     proc("vrServicio") = CInt(dr2("valorServicio"))
                     proc("conceptoRecaudo") = dr2("conceptoRecaudo").ToString()
                     proc("valorPagoModerador") = CInt((dr2("valorPagoModerador")))
